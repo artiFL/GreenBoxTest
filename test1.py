3c9356a1e970b4fe2a340dc0ce681259951168a2
flegler.a <flegler.a@future-tech.ru>
2022-08-03 18:27:37 +0700

    Version 2.5

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import serial
 from colorama import Fore
 from serial.tools import list_ports
@@ -7,23 +8,24 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing   
 from matplotlib import cm
 import os
+from pathlib import Path
 
 
-path_to_save_file_source = "C:/Users/flegler.a/Desktop/Work_space/CubeIDE/Checker/GreenBoxPython/GreenBoxTest/Data"
-#path_to_save_file_backup = os.getcwd()
-#print(path_to_save_file_backup)
-Channel_Checker = 3
-TimeStamp = 2
+path_to_save_file_source = str(Path.cwd()) + "\Data\\"
+SFP_PORT = 3
+TimeStamp = 0.7
 Speed = 0
 
+xsize = 26
+ysize = 26
 
 x = 10
 p = -15
 pre_cursor = []
 post_cursor = []
 buff_str_var = []
-BERT_Value = np.empty((26, 26))
-BERT_Normalize_Value = np.empty((26, 26))
+BERT_Value = np.empty((ysize, xsize))
+BERT_Normalize_Value = np.empty((ysize, xsize))
 Build_time = ""
 
 
@@ -42,18 +44,17 @@ def normalize(ptr):
     return BERT_Normalize_Value
 
 while x > -16:
-
     pre_cursor.append(str(x))
     post_cursor.append(str(p))
-
     p += 1
     x -= 1
-
 
 def main():
     buff_str_var = []
     nameModule = ""
 
+
+    print(path_to_save_file_source)
     Checker = serial.Serial(getport(), 115200)  # open serial port
 
     Checker.write(bytes("cmd_get_version", encoding="utf-8")) 
@@ -71,11 +72,11 @@ def main():
         print(Fore.RED + f"Version Sowftware - {version_Checker[0]}\nVersion Hardware - {version_Checker[1]}\nSerial Number - {version_Checker[2]}")
         print(Fore.WHITE + "Succes connect")
 
-    path_to_save_file_backup = path_to_save_file_source + str(version_Checker[2]) + "_Channel" + str(Channel_Checker)
+    path_to_save_file_backup = path_to_save_file_source + str(version_Checker[2]) + "_Channel" + str(SFP_PORT)
 
-    time.sleep(1)
-    Checker.write(bytes(f"cmd_start_callib_FIR {Channel_Checker} {TimeStamp} {Speed}", encoding="utf-8")) 
-    time.sleep(1)
+    #time.sleep(1)
+    Checker.write(bytes(f"cmd_start_callib_FIR {SFP_PORT} {TimeStamp} {Speed}", encoding="utf-8")) 
+    #time.sleep(1)
 
     flag = True
     ack_speed = ""
@@ -87,12 +88,11 @@ def main():
         if "Clock is set" in ack_speed:
             flag = False
 
-    nameModule = str(Checker.readline(), 'UTF-8')
+    ack_speed = ack_speed.split(',')
+    nameModule = ack_speed[2].replace('\n', '')
 
-    print(ack_speed)
-
-    for b in range(2):
-        for n in range(2):
+    for b in range(ysize):
+        for n in range(xsize):
             string_reccive = str(Checker.readline(), 'UTF-8').replace('\n', '')
             string_reccive = string_reccive.split(',')
 
@@ -108,10 +108,8 @@ def main():
     # save array to the file
     np.save(file, BERT_Value)
     print("File save to - " + path_to_save_file_backup + ".txt")
-
     # close the file
     file.close
-
 
     fig, ax = plt.subplots()
     im = ax.imshow(normalize(BERT_Value), cmap = cm.magma)
@@ -123,14 +121,16 @@ def main():
     # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
              rotation_mode="anchor")
+    plt.xlabel("post-cursor")
+    plt.ylabel("pre-cursor")
 
-    ax.set_title(f"Fir sweep {nameModule}")
+
+    ax.set_title(f"Fir sweep {nameModule} speed = {Speed} timestamp = {TimeStamp}")
     fig.tight_layout()
     plt.show()
 
-    fig.savefig(path_to_save_file_backup)
+    fig.savefig(path_to_save_file_backup + "_Figure")
 
 
 if __name__ == "__main__":
-    
     main()
